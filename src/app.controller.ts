e0257e24ab4755c652916b36fa459e6cfffaf8a7
I Am ByeBlogs <iam.byeblogs@gmail.com>
2023-07-28 21:58:11 +0700

    feat: add social login facebook and google

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Request } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FacebookAuthResult, GoogleAuthResult, UseFacebookAuth, UseGoogleAuth } from '@nestjs-hybrid-auth/all';
 
 @Controller()
 export class AppController {
@@ -14,4 +15,41 @@ export class AppController {
   getHelloName(@Param('name') name: string): string {
     return this.appService.getHelloName(name);
   }
+
+  @UseFacebookAuth()
+  @Get('auth/facebook')
+  loginWithFacebook() {
+    return 'Login with Facebook';
+  }
+
+  @UseFacebookAuth()
+  @Get('auth/facebook-login/callback')
+  facebookCallback(@Request() req): Partial<FacebookAuthResult> {
+    const result: FacebookAuthResult = req.hybridAuthResult;
+    console.log('FacebookAuthResult : ', result);
+    return {
+      accessToken: result.accessToken,
+      refreshToken: result.refreshToken,
+      profile: result.profile,
+    };
+  }
+
+  @UseGoogleAuth()
+  @Get('auth/google')
+  loginWithGoogle() {
+    return 'Login with Google';
+  }
+
+  @UseGoogleAuth()
+  @Get('auth/google-login/callback')
+  googleCallback(@Request() req): Partial<GoogleAuthResult> {
+    const result: GoogleAuthResult = req.hybridAuthResult;
+    console.log('GoogleAuthResult : ', result);
+    return {
+      accessToken: result.accessToken,
+      refreshToken: result.refreshToken,
+      profile: result.profile,
+    };
+  }
+
 }
